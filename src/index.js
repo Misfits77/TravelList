06fc2ce09fe9ba6da50bd256f361a8a03af0c4e8
Belen Lapadat <belenlapadat@gmail.com>
2022-11-30 18:49:45 -0300

    Fixed css styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import seed from "./seed";
 
 seed();
@@ -11,9 +11,9 @@ seed();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
